3a9238d7734a5d0f1f3dc286fa389368c3d0659e
Kantanand <kantanand.usk@gmail.com>
2016-06-08 17:59:52 +0530

    installed django

---
+++ djenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kantanand/code/python/nfdb_debug/django-portable/djenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
