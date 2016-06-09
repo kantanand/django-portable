e3246a00ece0c81d6e99bda71293bb63b4f62a0e
Kantanand <kantanand.usk@gmail.com>
2016-06-09 10:27:28 +0530

    deleting djenv

--- djenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/kantanand/code/python/nfdb_debug/django-portable/djenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
